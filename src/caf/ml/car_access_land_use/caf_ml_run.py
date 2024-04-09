f655292119cedf3978ea8bdb2647d5b40469c244
AdilZ16 <113607872+AdilZ16@users.noreply.github.com>
2024-04-09 21:58:45 +0100

    Update caf_ml_run.py

--- src/caf/ml/car_access_land_use/caf_ml_run.py
+++ src/caf/ml/car_access_land_use/caf_ml_run.py
@@ -1,64 +1,156 @@
 import sys
+import numpy as np
+import pandas as pd
+
 sys.path.extend(r"C:\Users\Liberty\Documents\GitHub\caf.ml\src")
 import warnings
+
 warnings.filterwarnings("ignore")
 
 from pathlib import Path
 from caf.ml.inputs.cafml_inputs import CarAccessInputs
 from caf.ml.car_access_land_use.process_data_class import DataProcessor
-from caf.ml.car_access_land_use.feature_selection import feature_selection_, apply_feature_selection
-from caf.ml.functions.feature_selection_hyper_optim import feature_selection_with_optimization
+from caf.ml.car_access_land_use.feature_selection import feature_selection_cv, filter_data
 from caf.ml.functions.hyper_optim_gridsearch import select_param
+from caf.ml.functions.forecast_model_functions import predict, process_data_loaded_model, \
+    process_forecast_data, match_columns
+from caf.ml.functions.save_model import save_model_and_parameters, load_model_and_parameters
+from caf.ml.functions.pre_forecast_data_analysis import pre_forecast_data_analysis
+
+
+# todo add interpolation function from lvu to dataprocessor class
+# todo refer to flow in note book for final forecasting part
 
 
 def main(params: CarAccessInputs):
-    ######## PROCESS DATA #########
-    processed_data = DataProcessor(params.x_path,
-                                   params.y_path,
-                                   params.folder_path,
-                                   params.index_columns,
-                                   params.drop_columns,
-                                   params.keep_columns,
-                                   params.target_column,
-                                   params.output_folder,
-                                   params.wide_format,
-                                   params.variable_name,
-                                   params.value_name,
-                                   params.outlier_threshold,
-                                   ).data
-
-
-    #todo add interpolation function from lvu to dataprocessor class
-    ######## SIMPLE FEATURE SELECTION #########
     if params.simple_feature_selection is not None:
-        # (30 min run time) non grid search feature selection
-        selected_feature_indices = feature_selection_(processed_data,
-                                                      params.target_column,
-                                                      params.model_type,
-                                                      params.cv_method,
-                                                      params.splits,
-                                                      params.repeats)
-        df_final = apply_feature_selection(selected_feature_indices)
-        print("Best features for model determined to be: %s", df_final)
-        model = select_param(df_final,
-                             params.target_column,
-                             params.model_type)
 
-    ######## FEATURE SELECTION WITH HYPERPARAMETER OPTIMISATION ########
-    elif params.simple_feature_selection is None:
-        model, df_final = feature_selection_with_optimization(processed_data,
-                                                              params.splits,
-                                                              params.repeats,
-                                                              params.model_type,
-                                                              params.target_column,
-                                                              params.cv_method)
-        print("Best features for model determined to be: %s", df_final)
-        print("Best model determined to be: %s", model)
+        ######## MODEL AND DATA SAVED ########
+        if params.saved_model is not None:
+
+            #### LOAD MODEL AND DATA ####
+            regression_method, hyperparameters, trained_data = load_model_and_parameters(
+                params.output_folder)
+
+            print(regression_method)
+            print(type(regression_method))
+
+            trained_data = process_data_loaded_model(df=trained_data,
+                                                     index_columns=params.index_columns,
+                                                     drop_columns=params.drop_columns,
+                                                     target_column=params.target_column,
+                                                     keep_columns=params.keep_columns,
+                                                     outlier_threshold=params.outlier_threshold)
+
+            predict_data = process_forecast_data(df=params.predict_data,
+                                                 index_columns_p=params.index_columns_predict,
+                                                 drop_columns_p=params.drop_columns_predict,
+                                                 target_column=params.target_column,
+                                                 keep_columns_p=params.keep_columns_predict,
+                                                 outlier_threshold_p=params.outlier_threshold_predict)
+
+            print(trained_data)
+            print(trained_data.shape)
+            print(trained_data.dtypes)
+            print(predict_data)
+            print(predict_data.shape)
+            print(predict_data.dtypes)
+
+            predict_data = match_columns(trained_data=trained_data, predict_data=predict_data)
+
+            #### FINAL FORECAST/PREDICT ####
+            forecasted_data = predict(params.single_year_prediction,
+                                      trained_data=trained_data,
+                                      predict_data=predict_data,
+                                      trained_model=regression_method,
+                                      target_column=params.target_column,
+                                      vif_threshold=params.threshold,
+                                      threshold_corr=params.threshold_corr,
+                                      output_folder=params.output_folder,
+                                      FinalModelParameters=hyperparameters)
+
+            print(forecasted_data)
+
+        ######## MODEL AND DATA NOT SAVED ########
+        elif params.saved_model is None:
+
+            #### PROCESS DATA #####
+            processed_data = DataProcessor(params.x_path,
+                                           params.y_path,
+                                           params.folder_path,
+                                           params.index_columns,
+                                           params.drop_columns,
+                                           params.keep_columns,
+                                           params.target_column,
+                                           params.output_folder,
+                                           params.wide_format,
+                                           params.variable_name,
+                                           params.value_name,
+                                           params.outlier_threshold
+                                           ).data
+
+            print('Checking if data is numeric')
+            all_numeric = processed_data.applymap(np.isreal).all().all()
+            print(all_numeric)
+
+            #### FEATURE SELECTION #####
+            selected_features_model, selected_f_classif, selected_rfe, model = feature_selection_cv(
+                data=processed_data,
+                model_type=params.model_type,
+                cv_method=params.cv_method,
+                splits=params.splits,
+                repeats=params.repeats,
+                target_column=params.target_column)
+
+            data = filter_data(original_data=processed_data,
+                               best_features_model=selected_features_model,
+                               best_features_f_classif=selected_f_classif,
+                               best_features_rfe=selected_rfe,
+                               output_folder=params.output_folder,
+                               target_column=params.target_column)
+
+            #### FINAL DATA ANALYSIS ####
+            df_final_to_model = pre_forecast_data_analysis(data=data,
+                                                           regression_method=model,
+                                                           target_column=params.target_column,
+                                                           threshold=params.threshold,
+                                                           threshold_corr=params.threshold_corr,
+                                                           output_folder=params.output_folder)
+
+            print("Final data to be modelled:")
+            print(df_final_to_model)
+            print('Conducting hyperparamter optimisation')
+
+            #### HYPERPARAMETER OPTIMISATION #####
+            hyperparameters = select_param(data=df_final_to_model,
+                                           target_column=params.target_column,
+                                           regression_method=model,
+                                           model_type=None)
+
+            #### SAVE MODEL #####
+            save_model_and_parameters(model,
+                                      hyperparameters,
+                                      output_folder=params.output_folder)
+
+            #### FINAL PREDICTION #####
+            forecasted_data = predict(params.single_year_prediction,
+                                      data_p=params.predict_data,
+                                      index_columns_p=params.index_columns_predict,
+                                      drop_columns_p=params.drop_columns_predict,
+                                      target_column=params.target_column,
+                                      keep_columns_p=params.keep_columns_predict,
+                                      outlier_threshold_p=params.outlier_threshold_predict,
+                                      FinalDataframe=df_final_to_model,
+                                      FinalModel=regression_method,
+                                      FinalModelParameters=hyperparameters,
+                                      output_folder=params.output_folder,
+                                      year_range=params.year_range,
+                                      vif_threshold=params.threshold,
+                                      threshold_corr=params.threshold_corr)
+
+            print(forecasted_data)
+
     warnings.filterwarnings("default")
-
-    ######## FINAL MODEL FORECAST ########
-
-
 
 
 if __name__ == "__main__":
@@ -83,7 +175,7 @@ if __name__ == "__main__":
                           :param value_name: name of column that will contain 
                           the values associated with melted dataframe
                           see https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.melt.html#pandas.melt
-                          
+
     --------------------------------------------------------------------------                     
     Algorithim Evaluation:
     :param models_to_test: A list of models to evaluate. The best model for the
@@ -106,7 +198,7 @@ if __name__ == "__main__":
     :param grid_search: Either none or yes. If yes, grid search feature selection
                         conducted. If none then alterantive feature selection
                         (selectfrommodel, pvalue, recursive)
-    
+
     If grid_search none: 
         :param target_column: the column (title) we want to forecast
         :param model_type: Algorthim used within the cross validation element
@@ -137,48 +229,60 @@ if __name__ == "__main__":
 
 
     If grid_search yes: 
-    
-    
-    
-    
-    
+
     '''
     params = CarAccessInputs(
-                        #### PROCESS DATA ####
-                        # imports
-                        x_path=Path(r"E:\caf.ml\data_process_function\test_data\cb_tfn_v12.csv"),
-                        y_path=None,
-                        folder_path=None,
+        #### PROCESS DATA ####
+        # imports
+        x_path=Path(r"E:\caf.ml\Prediction_Model\test_data\census_2011.csv"),
+        y_path=None,
+        folder_path=None,
 
-                        # data sorting imports
-                        index_columns=None,
-                        drop_columns=None,
-                        keep_columns=None,
-                        target_column=None,
-                        output_folder=None,
+        # data sorting imports
+        index_columns=['geo_code'],
+        drop_columns=['sum of all cars or vans in the area'],
+        keep_columns=None,
+        target_column='No cars or vans in household',
+        output_folder=Path(r"E:\caf.ml\Prediction_Model\test_data\output"),
 
-                        # wide to long imports
-                        wide_format=None,
-                        variable_name=None,
-                        value_name=None,
+        # wide to long imports
+        wide_format=None,
+        variable_name=None,
+        value_name=None,
 
-                        # optional imports
-                        outlier_threshold=None,
+        # optional imports
+        outlier_threshold=None,
 
+        #### FEATURE SELECTION ####
+        # feature selection methods
+        simple_feature_selection='yes',
+        feature_selection_exhaustive=None,
 
-                        #### FEATURE SELECTION ####
-                        simple_feature_selection=None,
-                        model_type=None,
-                        cv_method=None,
-                        splits=None,
-                        repeats=None,
+        # feature selection processing
+        process_data_used=True,
+        model_type=None,
+        cv_method=None,
+        splits=None,
+        repeats=None,
 
+        #### PREDICT DATA ####
+        single_year_prediction='yes',
+        predict_data=Path(r"E:\caf.ml\Prediction_Model\test_data\census_2021.csv"),
+        year_range=None,
 
+        # data sorting imports for predict data
+        index_columns_predict=['geo_code'],
+        drop_columns_predict=['sum of all cars or vans in the area'],
+        keep_columns_predict=None,
+        outlier_threshold_predict=None,
 
+        #### STORE MODEL ####
+        # MODEL_FILE_PATH = r"E:\caf.ml\Prediction_Model\test_data\saved_model\model.pkl"
+        # PARAMETERS_FILE_PATH = r"E:\caf.ml\Prediction_Model\test_data\saved_model\parameters.pkl"
+        saved_model=None,
 
+        threshold=10,
+        threshold_corr=0.7
 
-                        hp_optimisation=None,
-                        # output file path
-                        folder=Path(r"E:\caf.ml\data_process_function\car_model_results"),
     )
     main(params)

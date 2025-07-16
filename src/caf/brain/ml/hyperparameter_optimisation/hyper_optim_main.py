7dd25f652cb3cda4afa842e72f6d983b19fbe4e0
AdilZ16 <113607872+AdilZ16@users.noreply.github.com>
2025-07-16 11:23:13 +0100

    still updating

--- src/caf/brain/ml/hyperparameter_optimisation/hyper_optim_main.py
+++ src/caf/brain/ml/hyperparameter_optimisation/hyper_optim_main.py
@@ -1,61 +1,66 @@
-# -*- coding: utf-8 -*-
 """
 Created on: 1/21/2025
 Original author: Adil Zaheer
 """
-# pylint: disable=import-error,wrong-import-position
-# pylint: enable=import-error,wrong-import-position
 from caf.brain.ml.hyperparameter_optimisation.hyper_optim_functions import select_param
 from pathlib import Path
 import pandas as pd
 
+from caf.brain.ml.main_models.prediction_model.prediction_model_inputs import PredictionModelInputs
+
 
 def main_hyperparameter_optimisation(
-    train_final: pd.DataFrame,
-    target_column: str,
-    model_instance,
-    model_name,
-    classification_prediction: tuple[int, ...],
-    cv: str,
-    weight_column: str,
-    output_folder: Path,
-    is_time_series: bool,
+    paths: PredictionModelInputs.Paths,
+    data_classification: PredictionModelInputs.DataClassificationInputs,
+    transforming_inputs: PredictionModelInputs.TransformingInputDataInputs,
+    modelling: PredictionModelInputs.ModellingInputs,
+    train_final: pd.DataFrame = None,
+    model_instance=None,
+    output_folder: Path = None,
 ):
     """
     Main function for hyperparameter optimisation.
 
-    :param train_final: Dataframe of final training data post feature selection.
-    :param target_column: String column name of value to predict.
-    :param model_instance: Initialised model algorithm from Models enum class.
-    :param model_name: List or one algorithm to use as the base of the model.
-                       Available algorithms can be seen in prediction_model_inputs.py
-                       or __info__.py.
-    :param classification_prediction: List of integers that correspond to the
-                                      target column. The value(s) to predict
-                                      in a classification problem.
-    :param cv: Cross validation method passed as a string. Any popular
-               SciKitlearn methods are suitable with KFold being default if
-               left as None.
-    :param weight_column: Optional string column value to be used as weight.
-    :param output_folder: Path to output location.
-    :param is_time_series: If true then data must be time series. Time series
-                           based characteristics are taken into consideration
-                           during function execution.
+    Parameters
+    ----------
+    paths: Path inputs from the PredictionModelInputs class. These inputs
+           define paths to external files. See
+           caf/brain/ml/main_models/prediction_model/prediction_model_inputs.py
+           for available options.
+    data_classification: Data classification inputs from the PredictionModelInputs
+                         class. These inputs help define and outline the
+                         structure of the input data. See
+                         caf/brain/ml/main_models/prediction_model/prediction_model_inputs.py
+                         for available options.
+    transforming_inputs: Transforming inputs from the PredictionModelInputs
+                         class. These inputs dictate how the data is transformed
+                         for machine learning modelling. See
+                         caf/brain/ml/main_models/prediction_model/prediction_model_inputs.py
+                         for available options.
+    modelling: Modelling inputs from the PredictionModelInputs
+               class. These inputs control the machine learning modelling
+               pipeline and functions. See
+               caf/brain/ml/main_models/prediction_model/prediction_model_inputs.py
+               for available options.
+    train_final: Dataframe of final training data post feature selection.
+    model_instance: Initialised model algorithm from Models enum class.
+    output_folder: Path to output location.
 
-    :return:
-        best_model: Fitted final model for prediction on unseen (test) data.
+    Returns
+    -------
+    best_model: Fitted final model for prediction on unseen (test) data.
     """
 
     best_model = select_param(
         train_final=train_final,
-        target_column=target_column,
+        target_column=data_classification.target_column,
         model_instance=model_instance,
-        model_name=model_name,
-        classification_prediction=classification_prediction,
-        cv=cv,
-        weight_column=weight_column,
+        model_name=modelling.model_choice,
+        classification_prediction=transforming_inputs.classification_prediction,
+        cv=modelling.cv,
+        weight_column=data_classification.weight_column,
         output_folder=output_folder,
-        is_time_series=is_time_series,
+        is_time_series=data_classification.is_time_series,
     )
-
+    # todo return not just the best model but also the best grid as a dictionary for people to use?
     return best_model

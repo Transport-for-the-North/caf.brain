5e4446c042e13d555a627168969560e762c19160
Ben <68055450+BenTaylor-TfN@users.noreply.github.com>
2025-08-08 15:03:57 +0100

    rename test file

--- src/caf/brain/ml/main_models/prediction_model/__run__.py
+++ src/caf/brain/ml/main_models/prediction_model/__run__.py
@@ -11,7 +11,7 @@ import yaml
 from caf.toolkit import LogHelper, ToolDetails
 
 # Local Imports
-from caf.brain.ml.main_models.prediction_model.__testing_doc__ import main_test
+from caf.brain.ml.main_models.prediction_model.__example_doc__ import main as example_main
 from caf.brain.ml.main_models.prediction_model.inputs import (
     Models,
     PredictionModelInputs,
@@ -44,7 +44,7 @@ def model_setup():
     details = ToolDetails("caf.brAIn Prediction Model", "1.0.0")
 
     with LogHelper("caf.brain", details, console=True, log_file=path):
-        main_test(params, output_path)
+        example_main(params, output_path)
 
 
 if __name__ == "__main__":

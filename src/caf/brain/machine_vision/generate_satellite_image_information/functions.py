200b846ed54e1832a8f83079b429aa5cfc937582
AdilZ16 <113607872+AdilZ16@users.noreply.github.com>
2025-09-25 11:30:50 +0100

    refactored image information generation
    These functions generate satellite image metadata that is required to eventually source and crop images as we use a subset of a British National Grid tile. Initial linting and documentation done for these functions too

--- src/caf/brain/machine_vision/generate_satellite_image_information/functions.py
+++ src/caf/brain/machine_vision/generate_satellite_image_information/functions.py
@@ -2,28 +2,26 @@
 Created on: 9/17/2025
 Original author: Adil Zaheer
 """
-
-
-import pandas as pd
 from pathlib import Path
-from bs4 import BeautifulSoup
 import re
-from pyproj import Transformer
+import logging
 import os
 import glob
+import pandas as pd
+from bs4 import BeautifulSoup
+from pyproj import Transformer
 from tqdm import tqdm
-import logging
 LOG = logging.getLogger(__name__)
 
 
-def satellite_xml_processor(folder_path: Path,
+def satellite_xml_processor(image_folder_path: Path,
                             output_path: Path) -> pd.DataFrame:
     """
-    Creates a dataframe of satellite image xml data.
+    Creates a dataframe of British National Grid satellite image xml data.
 
     Parameters
     ----------
-    folder_path: Path to folder that contains xml files.
+    image_folder_path: Path to folder that contains xml files.
     output_path: Path to output location.
 
     Returns
@@ -35,10 +33,10 @@ def satellite_xml_processor(folder_path: Path,
 
     data_dict = {}
     counter = 0
-    paths = glob.glob(os.path.join(folder_path) + '/**/*.xml', recursive=True)
+    paths = glob.glob(os.path.join(image_folder_path) + '/**/*.xml', recursive=True)
     with tqdm(total=None) as pbar:
         for path in paths:
-            df, name = extract_info_from_html(html_file_path=path)
+            df, name = extract_info_from_xml(html_file_path=path)
             if df is None or df.empty:
                 LOG.warning("Skipping file %s due to missing or empty data.", path)
                 problem_html.append(path)
@@ -58,16 +56,21 @@ def satellite_xml_processor(folder_path: Path,
     return final_data
 
 
-def extract_info_from_html(html_file_path: Path) -> tuple:
+def extract_info_from_xml(html_file_path: str) -> tuple:
     """
+    Extracting key information from British National Grid xml files.
 
-    :param html_file_path:
+    Parameters
+    ----------
+    html_file_path: path to image xml file.
 
-    :return:
+    Returns
+    -------
+    df: dataframe of xml data information for a specific British National Grid image tile.
+    location: string location of the processed British National Grid image tile.
     """
     df = None
     location = None
-    data = []
     with open(html_file_path, 'r', encoding='utf-8') as file:
         xml_data = file.read()
 
@@ -92,27 +95,35 @@ def extract_info_from_html(html_file_path: Path) -> tuple:
             })
 
             string = df.at[0, 'bng_location']
-            pattern = r'\w+'
-            substrings = re.findall(pattern, string)
+            substrings = re.findall(r'\w+', string)
             location = substrings[2]
             df['bng_location'] = location
 
             df = find_midpoint(df, location)
 
-            data.append(df)
-
         else:
-            LOG.warning(f"Box boundary string was not found in the xml file for \
-                          {html_file_path}.")
+            LOG.warning("Box boundary string was not found in the xml file for %s.", html_file_path)
 
     else:
-        LOG.warning(f"Box boundary string was not found in the xml file for \
-                      {html_file_path}.")
+        LOG.warning("Box boundary string was not found in the xml file for %s.", html_file_path)
 
     return df, location
 
 
-def find_midpoint(df, location):
+def find_midpoint(df: pd.DataFrame, location: str) -> pd.DataFrame:
+    """
+    Finding the midpoint of a British National Grid image tile.
+
+    Parameters
+    ----------
+    df: British National Grid tile dataframe based on xml data
+    location: string of which British National Grid tile location is being processed.
+
+    Returns
+    -------
+    result_df: DataFrame of a British National Grid tile metadata including
+               midpoint coordinates in GCS and National Grid formats.
+    """
     df_1 = df.copy()
 
     northing = df_1[df_1['box_boundary'].isin(['South', 'North'])]
@@ -130,7 +141,19 @@ def find_midpoint(df, location):
     return result_df
 
 
-def coordinate_converter(df):
+def coordinate_converter(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Converts coordinates between systems (latitude & longitude to easting &
+    northing).
+
+    Parameters
+    ----------
+    df: British National Grid midpoint tile location.
+
+    Returns
+    -------
+    final_df: Input dataframe with converted coordinates attached.
+    """
     # long / lat -> easting / northing
     df_ = df.copy()
 
@@ -165,7 +188,7 @@ def image_path_name_finder(folder_path: Path) -> pd.DataFrame:
 
     paths = glob.glob(os.path.join(folder_path) + '/**/*.jpg', recursive=True)
     for path in paths:
-        directory, file_name = os.path.split(path)
+        _, file_name = os.path.split(path)
         name = os.path.splitext(file_name)[0]
         meta_dict[name] = path
 
--- src/caf/brain/machine_vision/generate_satellite_image_information/main.py
+++ src/caf/brain/machine_vision/generate_satellite_image_information/main.py
@@ -2,30 +2,45 @@
 Created on: 9/17/2025
 Original author: Adil Zaheer
 """
-import os
-import pandas as pd
 import logging
 from pathlib import Path
-
+import os
+import pandas as pd
 from caf.brain.machine_vision.generate_satellite_image_information.functions import image_path_name_finder, satellite_xml_processor
 
 LOG = logging.getLogger(__name__)
 
 
 def image_info_generation(output_path: Path,
-                          image_folder_path: Path):
+                          image_folder_path: Path) -> pd.DataFrame:
+    """
+    Generates satellite image metadata for each British National Grid tile
+    in the image_folder_path.
+
+    Parameters
+    ----------
+    output_path: Path to output folder.
+    image_folder_path: Path to folder that contains British National Grid tile
+                       jpegs and their accompanying xml files.
+
+    Returns
+    -------
+    image_metadata: Satellite image metadata containing tile names, midpoints
+                    and path locations.
+
+    """
     df_filename = os.path.join(output_path, 'satellite_image_metadata.csv')
     if os.path.exists(df_filename):
         LOG.info('Satellite image metadata already exists and is being read in')
         image_metadata = pd.read_csv(df_filename)
     else:
-        image_xml_df = satellite_xml_processor(folder_path=image_folder_path,
+        image_xml_df = satellite_xml_processor(image_folder_path=image_folder_path,
                                                output_path=output_path)
 
         image_path_df = image_path_name_finder(folder_path=image_folder_path)
 
         image_metadata = pd.merge(image_xml_df, image_path_df, on='box_boundary', how='left')
         image_metadata.to_csv(df_filename, index=False)
-        LOG.info('Satellite image metadata saved to: %', df_filename)
+        LOG.info('Satellite image metadata saved here: %s', df_filename)
 
     return image_metadata

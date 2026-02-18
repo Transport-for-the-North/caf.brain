d182a04ccfd0ec08de18009d494f2f08e608c894
AdilZ16 <113607872+AdilZ16@users.noreply.github.com>
2026-02-18 11:27:20 +0000

    removed machine vision code and fixed three liniting errors

--- src/caf/brain/machine_vision/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on: 2/25/2025
-Original author: Adil Zaheer
-"""
-# pylint: disable=import-error,wrong-import-position
-# pylint: enable=import-error,wrong-import-position
--- src/caf/brain/ml/_functions/process_data_functions/split_data_into_ttv.py
+++ src/caf/brain/ml/_functions/process_data_functions/split_data_into_ttv.py
@@ -268,7 +268,10 @@ def simple_train_test_split(
 
 
 def sample_data(
-    x: pd.DataFrame, y: pd.DataFrame, weight: np.ndarray = None, is_time_series: bool = False
+    x: pd.DataFrame,
+    y: pd.DataFrame,
+    weight: np.ndarray | None = None,
+    is_time_series: bool = False,
 ):
     """
     Take a sample of data whilst maintaining temporal nature of data if
@@ -298,17 +301,6 @@ def sample_data(
         LOG.warning(
             "Dataset has %d rows. Sampling 500,000 rows for memory efficiency.", n_rows
         )
-
-        # if is_time_series:
-        #     sample_indices = x.index[-500000:]
-        # else:
-        #     sample_indices = x.sample(n=500000, random_state=42).index
-        #
-        # x_sample = x.loc[sample_indices]
-        # y_sample = y.loc[sample_indices]
-        # weight_sample = (
-        #     weight[x.index.get_indexer(sample_indices)] if weight is not None else None
-        # )
 
         if is_time_series:
             sample_positions = np.arange(n_rows - 500000, n_rows)

22cf984d709a7fa736f8727a307790d3e5c2d009
AdilZ16 <113607872+AdilZ16@users.noreply.github.com>
2026-02-20 08:13:51 +0000

    ran black on entire repo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,6 +103,7 @@ intersphinx_mapping = {
 }
 intersphinx_timeout = 30
 
+
 # -- Options for Todo extension ----------------------------------------------
 def get_env_bool(name: str, default: bool) -> bool:
     value = os.getenv(name, default)
